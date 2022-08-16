bbe2fd7ee22b4d4ffe073d1ac1d21a2664b143dc
olushola oludipe <shotown101@gmail.com>
2022-08-16 11:22:49 -0400

    created Hero

--- VoisaScript.js
+++ VoisaScript.js
@@ -1,5 +1,7 @@
 const menuButton = document.querySelector(".menu-btn"),
-  navigationList = document.querySelector(".nav-list");
+  navigationList = document.querySelector(".nav-list"),
+  heroContent = document.querySelector(".hero-content ");
+
 let stat = true;
 
 menuButton.addEventListener("click", openMenu);
@@ -12,3 +14,15 @@ function openMenu() {
     stat = true;
   }
 }
+
+window.addEventListener('resize', e =>{
+  let width = document.body.clientWidth;
+  console.log(width)
+  if(parseInt(width)>480){
+    heroContent.classList.add("show")
+    console.log("check")
+  }
+  else{
+    heroContent.classList.remove("show");
+  }
+})
