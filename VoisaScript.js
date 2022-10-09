5ee01342af64c2ee72d2f1d812e58e2b23f1be9d
olushola oludipe <shotown101@gmail.com>
2022-10-09 19:22:38 -0400

    desktop hero and about

--- VoisaScript.js
+++ VoisaScript.js
@@ -1,7 +1,8 @@
 const menuButton = document.querySelector(".menu-btn"),
   navigationList = document.querySelector(".nav-list"),
   navigationListItems = document.querySelector(".nav-list_item"),
-  heroContent = document.querySelector(".hero-content ");
+  heroContent = document.querySelector(".hero-content "),
+  aboutInfo = document.querySelector(".about--info h3");
 
 let stat = true;
 
@@ -15,17 +16,23 @@ function openMenu() {
     stat = true;
   }
 }
+// window.addEventListener("resize", () => {
+//   const text = aboutInfo;
+//   let width = document.body.clientWidth;
+//   const about = document.createElement('p')
+//   about.innerText = `
+//   VoisaScript is a cross-platform web & code-editor Extension that adds comments to your code, 
+//   but those comments are in audio format and not the traditional text format. 
+//   VoisaScript also makes these "audio comments" readily available on most web browsers 
+//   (for access on GitHub, GitLab and 
+//   other source control platforms) and code-editors (currently only on VS code, 
+//   but we're working on extending to other editors )
+//   `;
 
-window.addEventListener("resize", (e) => {
-  let width = document.body.clientWidth;
-  console.log(width);
-  if (parseInt(width) > 480) {
-    heroContent.classList.add("show");
-    console.log("check");
-  } else {
-    heroContent.classList.remove("show");
-  }
-});
+//   if (parseInt(width) > 1000) {
+//     aboutInfo.after(about);
+//   }
+// });
 
 // Swiper
 
